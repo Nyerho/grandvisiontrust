76e7e22017cd94fc06a7fe257208c68164d0a1db
User <user@example.com>
2026-07-07 12:09:29 +0100

    Fix whitespace between sections, improve Vercel handler

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 
+const { initPromise } = require('../server');
 const app = require('../server');
 
 // Export a Vercel-compatible handler that waits for DB initialization
 module.exports = async (req, res) => {
-  await require('../server').initPromise;
+  await initPromise;
   return app(req, res);
 };
